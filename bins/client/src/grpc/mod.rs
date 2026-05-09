f23f0bca6e2fed248c02a7a877b8ab858174eb97
CodeMarco05 <brandt.marco05@gmail.com>
2026-05-09 22:38:00 +0200

    proto files update

--- bins/client/src/grpc/mod.rs
+++ bins/client/src/grpc/mod.rs
@@ -1,3 +1,4 @@
+// needed structure because of the way the package is used in the prost auto gen part for tonic
 pub mod observer {
     pub mod v1 {
         include!(concat!(env!("OUT_DIR"), "/observer.v1.rs"));
