44147e42439cf8b5418234d8af46512f13fd095e
CodeMarco05 <brandt.marco05@gmail.com>
2026-05-18 23:17:28 +0200

    added test and passing for adding data to a file

--- bins/client/src/data_storage/file_format/header.rs
+++ bins/client/src/data_storage/file_format/header.rs
@@ -1,9 +1,12 @@
+use std::thread::sleep;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Header{
-    pub magic: [u8; 8],
-    pub version: u8,
-    pub pad: [u8; 4],
-    pub block_count: u32,
-    pub checksum: u32
+    magic: [u8; 8],
+    version: u8,
+    pad: [u8; 4],
+    block_count: u32,
+    checksum: u32
 }
 
 impl Header{
@@ -27,6 +30,17 @@ impl Header{
         hasher.update(&self.block_count.to_le_bytes());
         hasher.finalize()
     }
+    
+    fn update_checksum(&mut self) -> &u32 {
+        let mut hasher = crc32fast::Hasher::new();
+        hasher.update(&self.magic);
+        hasher.update(&[self.version]);
+        hasher.update(&self.pad);
+        hasher.update(&self.block_count.to_le_bytes());
+        
+        self.checksum = hasher.finalize();
+        &self.checksum
+    }
 
     pub fn to_bytes(&self) -> Vec<u8> {
         let mut bytes = Vec::new();
@@ -36,6 +50,32 @@ impl Header{
         bytes.extend_from_slice(&self.block_count.to_le_bytes());
         bytes.extend_from_slice(&self.checksum.to_le_bytes());
         bytes
+    }
+
+    pub fn magic(&self) -> &[u8;8] {
+        &self.magic
+    }
+
+    pub fn version(&self) -> &u8 {
+        &self.version
+    }
+
+    pub fn pad(&self) -> &[u8;4] {
+        &self.pad
+    }
+
+    pub fn block_count(&self) -> &u32 {
+        &self.block_count
+    }
+
+    pub fn increment_block_count(&mut self) -> &u32 {
+        self.block_count += 1;
+        self.update_checksum();
+        &self.checksum()
+    }
+    
+    pub fn checksum(&self) -> &u32 {
+        &self.checksum
     }
 
     pub fn verify(&self) -> bool {
--- bins/client/src/data_storage/file_format/mod.rs
+++ bins/client/src/data_storage/file_format/mod.rs
@@ -1,25 +1,51 @@
+use std::iter::Map;
+use std::slice::Iter;
 use crate::data_storage::file_format::block::Block;
+use crate::data_storage::file_format::error::MetricsFileFormatError;
 use crate::data_storage::file_format::header::Header;
 
 pub mod header;
 mod block;
 mod error;
 
-pub struct MetricsFileFormat {
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct MetricsFile {
     header: Header,
     blocks: Option<Vec<Block>>,
     checksum: u32,
 }
 
-impl MetricsFileFormat {
+impl MetricsFile {
     pub fn default() -> Self {
-        let mut mf = MetricsFileFormat {
+        let mut mf = MetricsFile {
             header: Header::default(),
             blocks: None,
             checksum: 0,
         };
         mf.checksum = mf.compute_checksum();
         mf
+    }
+
+    pub fn with_data<D>(data: Vec<D>) -> Result<Self, MetricsFileFormatError>
+    where D: Into<Vec<u8>> + 'static
+    {
+        let mut file = MetricsFile::default();
+        data.into_iter().map(|d| file.add_data_block(d)).collect::<Result<(), _>>()?;
+        Ok(file)
+    }
+
+    pub fn add_data_block<D>(&mut self, data: D) -> Result<(), MetricsFileFormatError>
+    where D: Into<Vec<u8>> + 'static
+    {
+        self.header.increment_block_count();
+        self.blocks = match self.blocks.take() {
+            None => Some(vec![Block::with_data(data)?]),
+            Some(mut blocks) => {
+                blocks.push(Block::with_data(data)?);
+                Some(blocks)
+            }
+        };
+        Ok(())
     }
 
     pub fn compute_checksum(&self) -> u32 {
@@ -34,24 +60,38 @@ impl MetricsFileFormat {
         }
         hasher.finalize()
     }
-
-    pub fn add_block() {
-
-    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::data_storage::file_format::MetricsFileFormat;
+    use crate::data_storage::file_format::MetricsFile;
 
     #[test]
-    fn test_default_generation(){
-        let bare_bone = MetricsFileFormat::default();
+    fn default_generation_test(){
+        let bare_bone = MetricsFile::default();
 
         let header_magic = vec![b'O', b'B', b'S', b'E', b'R', b'V', b'E', b'R'];
-        assert_eq!(header_magic, bare_bone.header.magic);
+        assert_eq!(header_magic, bare_bone.header.magic());
 
         assert_eq!(bare_bone.checksum, 558161692);
         assert!(bare_bone.blocks.is_none());
+    }
+
+    #[test]
+    fn generate_with_data_test(){
+        let data = vec![vec![1,2,3,4,5], vec![6,7,8,9,0], vec![11,12,13,14,15]];
+        let file = MetricsFile::with_data(data.clone()).unwrap();
+
+        let first_block_data = file.blocks.as_ref().unwrap().first().unwrap().data();
+        let second_block_data = file.blocks.as_ref().unwrap().get(1).unwrap().data();
+        let last_block_data = file.blocks.as_ref().unwrap().last().unwrap().data();
+
+        assert_eq!(data.first().unwrap(), first_block_data);
+        assert_eq!(data.get(1).unwrap(), second_block_data);
+        assert_eq!(data.last().unwrap(), last_block_data);
+
+        let blocks = file.blocks.as_ref().unwrap();
+        assert_eq!(blocks[0].data_type(), blocks[1].data_type());
+        assert_eq!(blocks[1].data_type(), blocks[2].data_type());
     }
 }
