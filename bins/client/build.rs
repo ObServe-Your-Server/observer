0dd282a44e6bd2ded4c99a4fc46518e5643e5b3b
CodeMarco05 <brandt.marco05@gmail.com>
2026-07-14 19:07:37 +0200

    protofiles and building

--- bins/client/build.rs
+++ bins/client/build.rs
@@ -8,6 +8,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         std::env::set_var("PROTOC", protoc);
         std::env::set_var("PROTOC_INCLUDE", include);
     }
-    configure().compile_protos(&["proto/observer/v1/base_conn.proto"], &["proto"]).unwrap();
+    configure()
+        .compile_protos(
+            &[
+                "proto/observer/v1/base_conn.proto",
+                "proto/observer/v1/metrics/cpu.proto",
+                "proto/observer/v1/metrics/memory.proto",
+                "proto/observer/v1/metrics/disk.proto",
+                "proto/observer/v1/metrics/network.proto",
+                "proto/observer/v1/metrics/system.proto",
+                "proto/observer/v1/metrics/process.proto",
+                "proto/observer/v1/metrics/container_runtime.proto",
+                "proto/observer/v1/metrics/speedtest.proto",
+            ],
+            &["proto"],
+        )
+        .unwrap();
     Ok(())
 }
